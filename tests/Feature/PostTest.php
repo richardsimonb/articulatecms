029c8146b18bd57ab37ef4809edc231e64087cee
richardsimonb <richardsimonb@gmail.com>
2021-06-02 14:15:01 +0700

    create test for post

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_post_index_can_be_rendered()
+    {
+        $response = $this->get('/posts');
+
+        $response->assertStatus(200);
+    }
+}
