91c3ef84ecd48ebfaa87775a5f6495511df009b9
richardsimonb <richardsimonb@gmail.com>
2021-06-08 15:44:37 +0700

    add test for category index

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -5,17 +5,15 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Category;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    use RefreshDatabase;
+
+    public function test_category_index_can_be_rendered()
     {
-        $response = $this->get('/');
+        $response = $this->get('/category');
 
         $response->assertStatus(200);
     }
