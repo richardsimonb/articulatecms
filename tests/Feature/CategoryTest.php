9f08179b6dcd7191c0e8075e9485762ba453a6ca
richardsimonb <richardsimonb@gmail.com>
2021-06-21 20:47:42 +0700

    added test for create category view

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -13,7 +13,14 @@ class CategoryTest extends TestCase
 
     public function test_category_index_can_be_rendered()
     {
-        $response = $this->get('/category');
+        $response = $this->get('/categories');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_post_create_can_be_rendered()
+    {
+        $response = $this->get('/categories/create');
 
         $response->assertStatus(200);
     }
