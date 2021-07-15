6ecfdb8029688076316522f8927a115d8dab616f
richardsimonb <richardsimonb@gmail.com>
2021-07-15 18:51:18 +0700

    edit post controller index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('posts.index',$posts);
+        return view('posts.index', compact('posts'));
     }
 
     /**
