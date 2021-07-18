edaacc07d7387801d5b5668e4d501504c26d142c
richardsimonb <richardsimonb@gmail.com>
2021-07-18 20:55:46 +0700

    updated category controller index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('categories.index',$categories);   
+        return view('categories.index', compact("categories"));   
     }
 
     /**
