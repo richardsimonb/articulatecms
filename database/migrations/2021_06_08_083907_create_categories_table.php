2f6b1aa4047256f9a3e509157333adaf67a084a7
richardsimonb <richardsimonb@gmail.com>
2021-07-19 16:17:40 +0700

    edit category table

--- database/migrations/2021_06_08_083907_create_categories_table.php
+++ database/migrations/2021_06_08_083907_create_categories_table.php
@@ -17,7 +17,7 @@ class CreateCategoriesTable extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('parent');
+            $table->foreignId('parent_id');
         });
     }
 
