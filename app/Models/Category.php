54a1a46146ce5be40481018de7341c79e9a5da3b
richardsimonb <richardsimonb@gmail.com>
2021-07-26 13:20:12 +0700

    edit category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,6 +15,11 @@ class Category extends Model
      */
     protected $fillable = [
         'name',
-        'parent',
     ];
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    
 }
