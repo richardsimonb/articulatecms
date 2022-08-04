c9e1415b0bb591d1ccec749f958ed34b26d71eb5
richardsimonb <richardsimonb@gmail.com>
2022-08-04 21:40:12 +0700

    added default for category model parent

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -22,7 +22,9 @@ class Category extends Model
      */
     public function parent()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'parent_id')->withDefault([
+            'name' => 'Parent',
+        ]);;
     }
 
 }
